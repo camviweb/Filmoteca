d9f6550ecc4571d3dad65bd6adfd7f3ebb63e9fc
Câm-Vi Nguyen <Cam-Vi.Nguyen@etu.univ-paris1.fr>
2024-11-24 17:02:08 +0100

    modif index

--- index.php
+++ index.php
@@ -5,14 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filmoteca</title>
+    <link rel="stylesheet" href="css.css" />
 </head>
 
 <body>
-    <h1>Filmoteca</h1>
+    <header>
+        <h1>Filmoteca</h1>
+    </header>
     <div class="menu">
         <a href="index.html">Accueil</a>
         <a href="films.php">Liste des films</a>
     </div>
+    <section>
+        <p>Ce site est ma collection de films.</p>
+    </section>
     <footer>
         <p>&copy;Câm-Vi Nguyen</p>
     </footer>
